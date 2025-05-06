0a4c0bdb1d2e3c045b84548844e68399606ab9d5
shihanliang <sliang1@ChicagoBooth.edu>
2025-05-06 05:19:38 +0000

    code

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,4 +4,14 @@ class ItemsController < ApplicationController
 
     render({ :template => "item_templates/list" })
   end
+  def create
+    new_item = Item.new
+    new_item.link_url = params.fetch("query_link_url")
+    new_item.link_description = params.fetch("query_link_description")
+    new_item.thumbnail_url = params.fetch("query_thumbnail_url")
+    new_item.save
+  
+    redirect_to("/")
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
 
   get("/", { :controller => "items", :action => "index" })
+  get("/backdoor", { :controller => "items", :action => "new" })
+  post("/insert_item", { :controller => "items", :action => "create" })
+
 end
